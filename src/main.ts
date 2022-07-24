da8b0344035aaeab0a37912d61b3a1ad05d29b84
Jeff-Chi <asnotracking@gmail.com>
2022-07-24 23:39:12 +0800

    upgrade vite version

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
 import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-// import router from './router'
 
-const app =  createApp(App).use(ElementPlus);//.mount('#app')
-
-app.mount('#app');
+createApp(App).mount('#app')
